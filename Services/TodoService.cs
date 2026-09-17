641ce74aac4b61bf996cb63b6398e9c65ffe2338
samuel trajano <samuel.trajano@espas.ch>
2026-09-17 13:08:58 +0200

    logic mostly implemented

--- Services/TodoService.cs
+++ Services/TodoService.cs
@@ -3,11 +3,11 @@ namespace TodoApp.Services;
 
 public class TodoService
 {
-    public List<TodoItem> Todos { get; } = new List<TodoItem>();
+    public List<TodoItem> Todos { get; } = new List<TodoItem>(); // List of all todo items managed by the service.
 
-    private int nextId = 1;
+    private int nextId = 1; // Tracks the next available ID for new todo items.
 
-    public void Add
+    public void Add // Adds a new todo item to the list.
     (
         string title,
         string description,
@@ -16,7 +16,7 @@ public class TodoService
     )
     
     {
-        TodoItem todo = new TodoItem
+        TodoItem todo = new TodoItem // Create a new todo item with the provided details.
         {
             Id = nextId,
             Title = title,
@@ -27,7 +27,31 @@ public class TodoService
 
         nextId++;
 
-        Todos.Add(todo);
+        Todos.Add(todo); // Add the newly created todo item to the list.
     }
 
+    public TodoItem? GetById(int id) // Retrieves a todo item by its ID, or null if not found.
+    {
+        return Todos.FirstOrDefault(todo => todo.Id == id); // Retrieves a todo item by its ID, or null if not found.
+    }
+
+    public void Update(TodoItem todo) // Updates an existing todo item in the list.
+    {
+        var existingTodo = GetById(todo.Id);
+        if (existingTodo is not null)
+        {
+            existingTodo.Title = todo.Title;
+            existingTodo.Description = todo.Description;
+            existingTodo.Status = todo.Status;
+            existingTodo.Deadline = todo.Deadline;
+        }
+    }
+    public void Delete(int id)
+    {
+        TodoItem? todo = GetById(id);
+        if (todo is not null)
+        {
+            Todos.Remove(todo);
+        }
+    }
 }
